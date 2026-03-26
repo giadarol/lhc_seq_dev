dc9d544dfd0a07c5cb1575b2b786660d647a015f
giadarol <giovanni.iadarola@cern.ch>
2026-03-26 09:41:46 +0100

    Rename also elements

--- 000_generate_xsuite_lattices.py
+++ 000_generate_xsuite_lattices.py
@@ -14,27 +14,21 @@ seq_text = seq_text.replace(' at=', 'at:=')
 
 lhc = xt.load(string=seq_text, format='madx', reverse_lines=['lhcb2'], _rbend_correct_k0=True)
 
-# Patch triplet
-lhc.vars.load(string='''
-kqx.l1             := kqx2a.l1           ;
-ktqx1.l1           := kqx1.l1-kqx2a.l1   ;
-ktqx3.l1           := kqx3.l1-kqx2a.l1   ;
-kqx.r1             := kqx2a.r1           ;
-ktqx1.r1           := kqx1.r1-kqx2a.r1   ;
-ktqx3.r1           := kqx3.r1-kqx2a.r1   ;
-kqx.l5             := kqx2a.l5           ;
-ktqx1.l5           := kqx1.l5-kqx2a.l5   ;
-ktqx3.l5           := kqx3.l5-kqx2a.l5   ;
-kqx.r5             := kqx2a.r5           ;
-ktqx1.r5           := kqx1.r5-kqx2a.r5   ;
-ktqx3.r5           := kqx3.r5-kqx2a.r5   ;
-''', format='madx')
-
 # Rename lines
 lhc.lines['b1'] = lhc.lines['lhcb1']
 lhc.lines['b2'] = lhc.lines['lhcb2']
 del lhc.lines['lhcb1']
 del lhc.lines['lhcb2']
+
+# Rename elements
+for bname in ['b1', 'b2']:
+    tt = lhc[bname].get_table()
+    tt_to_rename = tt.rows['.*/lhc'+bname]
+    for nn in tt_to_rename.name:
+        new_name = nn.replace('/lhc'+bname, '/'+bname)
+        lhc.new(new_name, nn)
+        lhc[bname].replace(nn, new_name)
+        del lhc.elements[nn]
 
 # Force k0_from_h to False (k0 are all provided)
 for nn in list(lhc.elements.keys()):
