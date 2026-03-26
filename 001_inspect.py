be146584c4724698333d803173acdd0a5a36a494
giadarol <giovanni.iadarola@cern.ch>
2026-03-26 13:16:00 +0100

    Axis of d2 in the right place

--- 001_inspect.py
+++ 001_inspect.py
@@ -20,6 +20,7 @@ k0_d1_var_name = 'kd1.r5'
 k0_d2_var_name = 'kd2.r5'
 angle_d1_var_name = 'ad1.r5'
 angle_d2_var_name = 'ad2.r5'
+sep_d2_r5 = 0.188
 orientation = 1 # 1 if b1 is going from inside to outside, -1 otherwise
 
 line = lhc.b1
@@ -63,17 +64,8 @@ for nn in [d1_name, d2_name]:
     line[nn].edge_exit_model = 'linear'
     line[nn].edge_entry_angle_fdown = np.arcsin(tw0['px', nn])
     line[nn].edge_exit_angle_fdown = -np.arcsin(tw0['px', nn + '>>1'])
-    line[nn].edge_entry_angle_fdown = 0
-    line[nn].edge_exit_angle_fdown = 0
-
-# # Introduce magnet curvatures
-# for nn, vv_ang in [d1_name, d2_name]:
-#     line[nn].k0 = 0
-#     line[nn].k0_from_h = True
-#     line[nn].rbend_compensate_sagitta = False
-#     line[nn].rbend_model = 'straight-body'
-
-
+    line[nn].rbend_compensate_sagitta = False
+    line[nn].rbend_model = 'straight-body'
 
 d1_angle_in = np.arcsin(tw0['px', d1_name])
 d2_angle_in  = np.arcsin(tw0['px', d2_name])
@@ -87,17 +79,52 @@ line[d1_name].rbend_angle_diff = d1_angle_out - d1_angle_in
 line[d2_name].rbend_angle_diff  = d2_angle_out  - d2_angle_in
 
 # Set rbend shifts
-line[d1_name].rbend_shift += line[d1_name]._x0_in - tw0['y', d1_name]
-line[d2_name].rbend_shift += line[d2_name]._x0_out - tw0['y', '_end_point']
+line[d1_name].rbend_shift = line[d1_name]._x0_in - tw0['x', d1_name]
+line[d2_name].rbend_shift = line[d2_name]._x0_out - tw0['x', '_end_point'] + sep_d2_r5 / 2
 
-# Restore expressions on angles
-lhc[angle_d1_var_name] = -orientation * line[d1_name].angle
-lhc[angle_d2_var_name] = orientation * line[d2_name].angle
-lhc[d1_name].angle = -lhc.ref[angle_d1_var_name]
-lhc[d2_name].angle = lhc.ref[angle_d2_var_name]
+# Adapt elements
+lhc['ad1.r5'] = -orientation * line[d1_name].angle
+lhc['ad2.r5'] = orientation * line[d2_name].angle
+lhc['sep_mid_d1.r5'] = 2 * line[d1_name].rbend_shift
+lhc['sep_mid_d2.r5'] = 2 * line[d2_name].rbend_shift
+
+lhc['mbxf.4r5/b1'].rbend_model = 'straight-body'
+lhc['mbxf.4r5/b1'].rbend_compensate_sagitta = False
+lhc['mbxf.4r5/b1'].angle = -lhc.ref['ad1.r5']
+# lhc['mbxf.4r5/b1'].rbend_angle_diff = -lhc.ref['ad1.r5']
+# lhc['mbxf.4r5/b1'].k0 = -lhc.ref['kd1.r5']
+# lhc['mbxf.4r5/b1'].rbend_shift = lhc.ref['sep_mid_d1.r5'] / 2
+# lhc['mbxf.4r5/b1'].edge_entry_angle_fdown = 0
+# lhc['mbxf.4r5/b1'].edge_exit_angle_fdown = -lhc.ref['ad1.r5']
+
+
+# lhc['mbrd.4r5.b1'].rbend_model = 'straight-body'
+# lhc['mbrd.4r5.b1'].rbend_compensate_sagitta = False
+# lhc['mbrd.4r5.b1'].angle = lhc.ref['ad2.r5']
+# lhc['mbrd.4r5.b1'].k0 = lhc.ref['kd2.r5']
+# lhc['mbrd.4r5.b1'].rbend_angle_diff = lhc.ref['ad2.r5']
+
+
+line.slice_thick_elements(
+        slicing_strategies=[
+            # Slicing with thin elements
+            xt.Strategy(slicing=None),
+            xt.Strategy(slicing=xt.Uniform(3, mode='thick'), name='mbx.*'),
+            xt.Strategy(slicing=xt.Uniform(3, mode='thick'), name='mbrd.*'),
+
+    ])
 
 sv = line.survey(element0=ip_name)
 
 
+import matplotlib.pyplot as plt
+plt.close('all')
+plt.figure(1)
+plt.plot(sv.Z, sv.X, label='X')
+plt.xlim(-180, 180)
+plt.ylim(-0.15, 0.15)
+plt.xlabel('Z [m]')
+plt.ylabel('X [m]')
+plt.legend()
 
 # k0_d1_name 
